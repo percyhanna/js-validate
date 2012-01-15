559e50a0bff45fdc15d94ebc6cf9a7e13dfaf236
Andrew Hanna <github@sevenlight.com>
2012-01-15 13:18:46 -0700

    Add validator namespace structure.

--- test/js-validate.spec.js
+++ test/js-validate.spec.js
@@ -2,7 +2,7 @@ var jsValidate = require('../lib/main.js');
 console.log(jsValidate);
 
 describe('validate', function() {
-  it('validates string values', function() {
-    expect(jsValidate.validate('a', 'String')).toBeTruthy();
-  });
+    it('validates string values', function() {
+        expect(jsValidate.validate('a', 'String')).toBeTruthy();
+    });
 });
